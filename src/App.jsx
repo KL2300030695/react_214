1c4ee47831c74d64ae9ad5cb14de2cddd9fe0257
Subhash Vadaparthi <2300030695cseh@gmail.com>
2025-07-26 16:31:50 +0530

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'; // move vite.svg to src/assets if needed
+import viteLogo from './assets/vite.svg';
 
 import './App.css'
 
